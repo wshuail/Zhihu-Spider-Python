dd814e2c517b534c8fd360ff1249bbde77c771fd
wangshuailong <oopswangsl@gmail.com>
2016-03-18 21:59:20 +0800

    fix some bugs

--- zhihu/user.py
+++ zhihu/user.py
@@ -21,168 +21,224 @@ sys.setdefaultencoding('utf-8')
 
 session = None
 
+
 class User(Question):
 
     def __init__(self, url):
         self.url = url
         self.soup = None
         self.profile_url = self.url + '/about'
-        self.url_suffix = re.match(r'.+/people/(.+)', self.url).group(1) 
+        self.url_suffix = re.match(r'.+/people/(.+)', self.url).group(1)
         self.ask_url = self.url + '/asks'
         self.answer_url = self.url + '/answers'
         self.post_url = self.url + '/posts'
         self.collection_url = self.url + '/collections'
         self.edit_url = self.url + '/logs'
-        self.ask_num = None
-        self.answer_num = None
-        self.post_num = None
-        self.collection_num = None
-        self.edit_num = None
+        self.ask_number = None
+        self.answer_number = None
+        self.post_number = None
+        self.collection_number = None
+        self.edit_number = None
+
+    def user_name(self):
+        if self.soup is None:
+            self.soup = self.parse(self.url)
+        name = self.soup.find('div', {'class': 'name'}).string
+        return name
+
+    def weibo(self):
+        if self.soup is None:
+            self.soup = self.parse(self.url)
+        doc = self.soup.find('a', {'class': 'zm-profile-header-user-weibo'})
+        if weibo is not None:
+            weibo = doc['href']
+        return weibo
+
 
     def signature(self):
-        if self.soup == None:
+        if self.soup is None:
             self.soup = self.parse(self.url)
-
-        signature = self.soup.find('div', {'class': 'zm-profile-header-main'}).find('span', {'class': 'bio'})['title']
+        doc = self.soup.find('span', {'class': 'bio'})
+        if doc is not None:
+            signature = doc['title']
+        else:
+            signature = None
         return signature
 
     def bussiness_domain(self):
-        if self.soup == None:
+        if self.soup is None:
             self.soup = self.parse(self.url)
-        bussiness_domain = self.soup.find('span', {'class': 'business item'})['title']
+        doc = self.soup.find(
+            'span', {'class': 'business'})
+        if doc is not None:
+            bussiness_domain = doc['title']
+        else:
+            bussiness_domain = None
         return bussiness_domain
 
     def gender(self):
-        if self.soup == None:
+        if self.soup is None:
             self.soup = self.parse(self.url)
-        gender_profile = self.soup.find('div', {'class': 'zm-profile-header-info'}).find('span', {'class': 'item gender'}).find('i')['class']
-        if gender_profile[1] == 'icon-profile-male':
-            gender = 'M'
-        elif gender_profile[1] == 'icon-profile-female':
-            gender = 'F'
+        doc = self.soup.find('span', {'class': 'gender'})
+        if doc is not None:
+            if doc.find('i')['class'][1] == 'icon-profile-male':
+                gender = 'M'
+            #elif gender_profile[1] == 'icon-profile-female':
+            else:
+                gender = 'F'
         else:
             gender = 'unknown'
-
         return gender
-    
+
     def location(self):
-        if self.soup == None:
+        if self.soup is None:
             self.soup = self.parse(self.url)
-        location = self.soup.find('span', {'class': 'location'})['title']
+        doc = self.soup.find('span', {'class': 'location'})
+        if doc is not None:
+            location = doc['title']
+        else:
+            location = None
         return location
 
     def company(self):
-        if self.soup == None:
+        if self.soup is None:
             self.soup = self.parse(self.url)
-        company = self.soup.find('span', {'class': 'employment'})['title']
+        doc = self.soup.find('span', {'class': 'employment'})
+        if doc is not None:
+            company = doc['title']
+        else:
+            company = None
         return company
 
     def positon(self):
-        if self.soup == None:
+        if self.soup is None:
             self.soup = self.parse(self.url)
-        position = self.soup.find('span', {'class': 'position'})['title']
+        doc = self.soup.find('span', {'class': 'position'})
+        if doc is not None:
+            position = doc['title']
+        else:
+            position = None
         return position
 
     def education(self):
-        if self.soup == None:
+        if self.soup is None:
             self.soup = self.parse(self.url)
-        education = self.soup.find('span', {'class': 'education'})['title']
+        doc = self.soup.find('span', {'class': 'education'})
+        if doc is not None:
+            education = doc['title']
+        else:
+            education = None
         return education
 
     def education_extra(self):
-        if self.soup == None:
+        if self.soup is None:
             self.soup = self.parse(self.url)
-        education_extra = self.soup.find('span', {'class': 'education-extra'})['title']
+        doc = self.soup.find('span', {'class': 'education-extra'})
+        if doc is not None:
+            education_extra = doc['title']
+        else:
+            education_extra = None
         return education_extra
 
     def following_num(self):
-        if self.soup == None:
+        if self.soup is None:
             self.soup = self.parse(self.url)
-        docs = self.soup.find('div', {'class': 'zu-main-sidebar'}).find_all('strong')
+        docs = self.soup.find(
+            'div', {'class': 'zu-main-sidebar'}).find_all('strong')
         following_num = docs[0].string
         follower_num = docs[1].string
 
         return following_num, follower_num
 
     def agree_num(self):
-        if self.soup == None:
+        if self.soup is None:
             self.soup = self.parse(self.url)
-        agree_num = self.soup.find('span', {'class': 'zm-profile-header-user-agree'}).strong.string
+        agree_num = self.soup.find(
+            'span', {'class': 'zm-profile-header-user-agree'}).strong.string
         return agree_num
-        
+
     def thank_num(self):
-        if self.soup == None:
+        if self.soup is None:
             self.soup = self.parse(self.url)
-        thank_num = self.soup.find('span', {'class': 'zm-profile-header-user-thanks'}).strong.string
+        thank_num = self.soup.find(
+            'span', {'class': 'zm-profile-header-user-thanks'}).strong.string
         return thank_num
 
     def user_visitor_num(self):
-        if self.soup == None:
+        if self.soup is None:
             self.soup = self.parse(self.url)
-        docs = self.soup.find('div', {'class': 'zu-main-sidebar'}).find_all('div', {'class': 'zm-profile-side-section'})
-        user_visitor_num = docs[len(user_visitor_num_docs) - 1].find('div', {'class': 'zm-side-section-inner'}).strong.string
+        docs = self.soup.find('div', {
+                              'class': 'zu-main-sidebar'}).find_all('div', {'class': 'zm-profile-side-section'})
+        user_visitor_num = docs[
+            len(docs) - 1].find('div', {'class': 'zm-side-section-inner'}).strong.string
         return user_visitor_num
 
     def ask_num(self):
-        if self.soup == None:
+        if self.soup is None:
             self.soup = self.parse(self.url)
-        ask_num = int(self.soup.find('a', {'class': 'item', 'href': re.compile(r'/people/.+/asks')}).find('span', {'class': 'num'}).string)
-        self.ask_num = ask_num
+        ask_num = int(self.soup.find('a', {'class': 'item', 'href': re.compile(
+            r'/people/.+/asks')}).find('span', {'class': 'num'}).string)
+        self.ask_number = ask_num
         return ask_num
 
     def answer_num(self):
-        if self.soup == None:
+        if self.soup is None:
             self.soup = self.parse(self.url)
-        answer_num = int(self.soup.find('a', {'class': 'item', 'href': re.compile(r'/people/.+/answers')}).find('span', {'class': 'num'}).string)
-        self.answer_num = answer_num
+        answer_num = int(self.soup.find('a', {'class': 'item', 'href': re.compile(
+            r'/people/.+/answers')}).find('span', {'class': 'num'}).string)
+        self.answer_number = answer_num
         return answer_num
 
     def post_num(self):
-        if self.soup == None:
+        if self.soup is None:
             self.soup = self.parse(self.url)
-        post_num = int(self.soup.find('a', {'class': 'item', 'href': re.compile(r'/people/.+/posts')}).find('span', {'class': 'num'}).string)
-        self.post_num = post_num
+        post_num = int(self.soup.find('a', {'class': 'item', 'href': re.compile(
+            r'/people/.+/posts')}).find('span', {'class': 'num'}).string)
+        self.post_number = post_num
         return post_num
 
     def collection_num(self):
-        if self.soup == None:
+        if self.soup is None:
             self.soup = self.parse(self.url)
-        collection_num = int(self.soup.find('a', {'class': 'item', 'href': re.compile(r'/people/.+/collections')}).find('span', {'class': 'num'}).string)
-        self.collection_num = collection_num
+        collection_num = int(self.soup.find('a', {'class': 'item', 'href': re.compile(
+            r'/people/.+/collections')}).find('span', {'class': 'num'}).string)
+        self.collection_number = collection_num
         return collection_num
 
     def edit_num(self):
-        if self.soup == None:
+        if self.soup is None:
             self.soup = self.parse(self.url)
-        edit_num = int(self.soup.find('a', {'class': 'item', 'href': re.compile(r'/people/.+/logs')}).find('span', {'class': 'num'}).string)
-        self.edit_num = edit_num
+        edit_num = int(self.soup.find('a', {'class': 'item', 'href': re.compile(
+            r'/people/.+/logs')}).find('span', {'class': 'num'}).string)
+        self.edit_number = edit_num
         return edit_num
 
     def user_ask(self):
-        if self.ask_num == None:
+        if self.ask_num is None:
             self.ask_num = self.ask_num()
         ask_link_list = []
         if self.ask_num > 0:
-            for i in range(1, int(self.ask_num/20) + 2):
+            for i in range(1, int(int(self.ask_number)/20) + 2):
                 ask_url = self.ask_url + '?page=' + str(i)
-                ask_soup = self.parse(self.ask_url)
+                ask_soup = self.parse(ask_url)
                 docs = ask_soup.find_all('h2')
                 for doc in docs:
-                    ask_link = 'https://www.zhihu.com' + str(doc.find('a', {'class': 'question_link'})['href'])
+                    ask_link = 'https://www.zhihu.com' + \
+                        str(doc.find('a', {'class': 'question_link'})['href'])
                     ask_link_list.append(ask_link)
         return ask_link_list
 
     def user_answer(self):
-        if self.answer_num == None:
+        if self.answer_num is None:
             self.answer_num = self.answer_num()
         answer_link_list = []
         if self.answer_num > 0:
             for i in range(1, int(self.answer_num/20) + 2):
                 answer_url = self.answer_url + '?page=' + str(i)
-                soup = self.parse(self.answer_url)
+                soup = self.parse(answer_url)
                 docs = soup.find_all('h2')
                 for doc in docs:
-                    link = 'https://www.zhihu.com' + str(user_answer_doc.find('a', {'class': 'question_link'})['href'])
+                    link = 'https://www.zhihu.com' + \
+                        str(doc.find('a', {'class': 'question_link'})['href'])
                     answer_link_list.append(link)
         return answer_link_list
-
